9852ea959c85b7bd293842ee6375324e44d50f27
Kit Kemper <cgkemper@gmail.com>
2018-11-21 11:57:29 -0500

    Initial commit.

---
+++ pydata.py
@@ -0,0 +1,34 @@
+# Chapter 2
+
+import pandas as pd
+
+unames = ['user_id', 'gender', 'age', 'occupation', 'zip']
+users = pd.read_table('pydata-book-master/ch02/movielens/users.dat', sep = '::', header=None, names=unames)
+
+rnames = ['user_id', 'movie_id', 'rating', 'timestamp']
+ratings = pd.read_table('pydata-book-master/ch02/movielens/ratings.dat', sep='::', header=None, names=rnames)
+
+mnames = ['movie_id', 'title', 'genres']
+movies = pd.read_table('pydata-book-master/ch02/movielens/movies.dat', sep='::', header=None, names=mnames)
+
+data = pd.merge(pd.merge(ratings, users), movies)
+
+mean_ratings = data.pivot_table('rating', index='title', columns='gender', aggfunc='mean')
+
+ratings_by_title = data.groupby('title').size()
+
+active_titles = ratings_by_title.index[ratings_by_title >= 250]
+
+mean_ratings = mean_ratings.ix[active_titles]
+
+top_female_ratings = mean_ratings.sort_values(by='F', ascending=False)
+
+mean_ratings['diff'] = mean_ratings['M'] - mean_ratings['F']
+
+sorted_by_diff = mean_ratings.sort_values(by='diff')
+
+sorted_by_diff[::-1][:15]
+
+rating_std_by_title = data.groupby('title')['rating'].std()
+rating_std_by_title = rating_std_by_title.ix[active_titles]
+rating_std_by_title.sort_values(ascending=False)
